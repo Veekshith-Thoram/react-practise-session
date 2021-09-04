bb18f2548869979211bf54c4b239866695a21406
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2021-09-04 23:31:53 +0530

    adding buttons

--- calc/src/App.js
+++ calc/src/App.js
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import { useState } from 'react'
 function App() {
+    const [result, setResult] = useState("hi");
+
     return (
         <div>
-            <h1>Hello Pushpa</h1>
+            <div className="container">
+                <form>
+                    <input type="text" value={result} />
+                </form>
+
+                <div className="keypad">
+                    <button>Clear</button>
+                    <button>C</button>
+                    <button>&divide;</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>&times;</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>&ndash;</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+</button>
+                    <button>.</button>
+                    <button>0</button>
+                    <button>=</button>
+                </div>    
+            </div>            
         </div>
     )
 }
